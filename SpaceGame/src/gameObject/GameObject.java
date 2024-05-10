8d1fc4fe91bf989458d8d9dde17c8058ef4d6f06
DiegoQuevedo38 <quevedodiego3008@gmail.com>
2024-05-10 17:16:39 +0200

    GameObject file modified

--- SpaceGame/src/gameObject/GameObject.java
+++ SpaceGame/src/gameObject/GameObject.java
@@ -7,22 +7,23 @@ import SpaceGame.src.math.Vector2D;
 
 public abstract class GameObject {
     protected BufferedImage texture;
-    protected Vector2D position;
+	protected Vector2D position;
+	
+	public GameObject(Vector2D position, BufferedImage texture)
+	{
+		this.position = position;
+		this.texture = texture;
+	}
+	
+	public abstract void update(float dt);
+	
+	public abstract void draw(Graphics g);
 
-    public GameObject(Vector2D position, BufferedImage texture){
-        this.position = position;
-        this.texture = texture;
-    }
+	public Vector2D getPosition() {
+		return position;
+	}
 
-    public abstract void update();
-
-    public abstract void draw (Graphics g);
-
-    public Vector2D getPosition() {
-        return position;
-    }
-
-    public void setPosition(Vector2D position){
-        this.position = position;
-    }
+	public void setPosition(Vector2D position) {
+		this.position = position;
+	}
 }
